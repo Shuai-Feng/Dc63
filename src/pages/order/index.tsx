b9db0b8d7699baa254960ebd5544396328b2b5bf
shuaifeng <shuaifeng121314@126.com>
2021-01-12 09:36:53 +0800

    orderpage初始结构完成搭建

---
+++ src/pages/order/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+interface IOrderPageProps {}
+
+const OrderPage: React.FunctionComponent<IOrderPageProps> = props => {
+  return <div className="OrderPage">OrderPage</div>;
+};
+
+export default OrderPage;
