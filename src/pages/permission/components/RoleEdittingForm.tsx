ee513d4e88fe24f1c88734579bb7a615736133e2
shuafieng <shuaifeng121314@126.com>
2021-02-10 11:37:40 +0800

    完成权限界面的编写

--- src/pages/permission/components/RoleEdittingForm.tsx
+++ src/pages/permission/components/RoleEdittingForm.tsx
@@ -1,46 +1,102 @@
 import * as React from 'react';
-import { Form, Input, Transfer } from 'antd';
+import { Form, Input, Transfer, message } from 'antd';
 import { FormComponentProps, FormItemProps } from 'antd/es/form';
 import FormItem from 'antd/lib/form/FormItem';
+import SFaxios from '@/utils/SFaxios';
 
 interface IRoleEditingFormProps extends FormComponentProps {
   getForm: Function;
   rowData: any;
 }
 
+//   /role/user_list 用户表单具体信息
+
 const RoleEditingForm: React.FunctionComponent<IRoleEditingFormProps> = props => {
   let { getFieldDecorator, setFields } = props.form;
   let { rowData } = props;
+
+  const [ts_dataSource, setDatasource] = React.useState<any>();
+  const [ts_tagetKey, setTargetKey] = React.useState<any>();
 
   React.useEffect(() => {
     //获取表单实例
     if (props.getForm) {
       props.getForm(props.form);
     }
+    handleRequest();
   }, []);
 
   const formatItems: FormItemProps = {
     labelCol: {
       sm: 24,
-      md: 6,
+      md: 4,
     },
     wrapperCol: {
       sm: 24,
-      md: 16,
+      md: 20,
     },
   };
 
-  debugger;
+  /** */
+  const handleRequest = async () => {
+    try {
+      let data: any[] = await (
+        await SFaxios.ajax({
+          url: '/role/user_list',
+          data: {
+            params: rowData.id,
+          },
+        })
+      ).result;
+      // Transfer 数据源
+      let datasource: any = [];
+      //targetKet transfer中选定内容
+      let targetkey: any = [];
+
+      data.forEach(item => {
+        const data = {
+          key: item.user_id,
+          title: item.user_name,
+          status: item.status,
+        };
+        if (data.status == 1) {
+          targetkey.push(data.key);
+        }
+        datasource.push(data);
+      });
+
+      setDatasource(datasource);
+      setTargetKey(targetkey);
+    } catch (e) {
+      message.info(e.message);
+    }
+  };
   return (
     <div className="RoleEditingForm">
       <Form>
-        <FormItem label="状态" {...formatItems}>
+        <FormItem label="角色状态" {...formatItems}>
           {getFieldDecorator('status', {
             initialValue: rowData.role_name,
           })(<Input disabled />)}
         </FormItem>
-        <FormItem {...formatItems}>
-          {getFieldDecorator('role_name', {})(<Transfer />)}
+        <FormItem label="人员控制" {...formatItems}>
+          {getFieldDecorator(
+            'role_name',
+            {},
+          )(
+            <Transfer
+              titles={['待选用户', '已选用户']}
+              dataSource={ts_dataSource}
+              targetKeys={ts_tagetKey}
+              render={(item: any) => {
+                return item.title;
+              }}
+              onChange={targetKey => {
+                setTargetKey(targetKey);
+              }}
+              style={{ width: 550 }}
+            />,
+          )}
         </FormItem>
       </Form>
     </div>
